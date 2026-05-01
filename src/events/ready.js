5bd79f83761a6122b216c12de88f3d0a4c57b33d
zikazodo <owner.zikazodo@gmail.com>
2026-05-01 18:39:07 +0300

    fix: status getting cleared

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,12 @@
 const CommandHandler = require('../handlers/CommandHandler');
 
+function setPresence(client) {
+  client.user.setPresence({
+    activities: [{ name: '.help | Created by difficultyy', type: 0 }],
+    status: 'online',
+  });
+}
+
 module.exports = {
   name: 'clientReady',
   once: true,
@@ -7,10 +14,11 @@ module.exports = {
     console.log(`\n🎰 Racket is online as ${client.user.tag}`);
     console.log(`📡 Serving ${client.guilds.cache.size} servers\n`);
 
-    client.user.setPresence({
-      activities: [{ name: '.help | Created by difficultyy', type: 0 }],
-      status: 'online',
-    });
+    // Set presence immediately
+    setPresence(client);
+
+    // Refresh presence every 30 minutes to prevent Discord from clearing it
+    setInterval(() => setPresence(client), 30 * 60 * 1000);
 
     // Register slash commands now that client is ready
     const handler = new CommandHandler(client);
